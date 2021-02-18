a2fbd4b3093b4ba5c1a76690d4532a3b78b80a83
Isaac Vitor <isaacvitor@gmail.com>
2021-02-18 17:42:48 -0300

    add ex01 scheduler

---
+++ tt01/ex01.py
@@ -0,0 +1,40 @@
+"""Using yield as a primitive coroutine"""
+from collections import deque
+
+
+def counter_up(stop):
+    cont = 1
+    while cont <= stop:
+        yield cont
+        cont += 1
+
+
+def counter_down(start):
+    while start >= 1:
+        yield start
+        start -= 1
+
+
+class Scheduler:
+    def __init__(self):
+        self.queue = deque()
+
+    def add_new(self, coroutine):
+        self.queue.append(coroutine)
+
+    def run(self):
+        while self.queue:
+            task = self.queue.popleft()
+            try:
+                result = next(task)
+                print(f'task={task.__name__}: result={result}')
+
+                self.queue.append(task)
+            except StopIteration:
+                print('Opss!')
+
+
+s = Scheduler()
+s.add_new(counter_up(10))
+s.add_new(counter_down(15))
+#s.run()
